26f2a65c788d305407ff0d6f610f79799eca093d
piatkiewicz <piatkiewicz.piotr@gmail.com>
2017-05-29 19:10:48 +0200

    Schowanie formularza nowego posta dla niezalogowanych

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,13 +4,14 @@
 
     <h2>Posty</h2>
 
-    <form>
-        <div class="form-group">
-            <textarea class="form-control" rows="3"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Dodaj wpis</button>
-    </form>
-    
+    @if (Auth::check())
+        <form>
+            <div class="form-group">
+                <textarea class="form-control" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Dodaj wpis</button>
+        </form>
+    @endif
 
     @foreach($posts as $post) 
         @include('partials.post')
